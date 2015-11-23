1edaabffa12e2eae1834e30faba18bf1896245f9
Fernando Blat <ferblape@gmail.com>
2015-11-23 17:52:20 +0100

    Detect locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,25 +8,20 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
   helper_method :current_user, :logged_in?, :current_user?, :login_path
 
-  rescue_from OAuth::Unauthorized, with: :external_service_unauthorized
+  private
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = session[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
+  rescue I18n::InvalidLocale
+    I18n.locale = I18n.default_locale
   end
 
-  def default_url_options(options={})
-    { locale: I18n.locale }
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first rescue nil
   end
 
   def render_404
     render file: "public/404", status: 404, layout: false, handlers: [:erb], formats: [:html]
-  end
-
-  protected
-
-  def external_service_unauthorized
-    flash[:error] = t('.unauthorized')
-    redirect_to root_path
   end
 
 end
